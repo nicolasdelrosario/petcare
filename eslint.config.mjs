00acdfd7020abc8b21333d938762bbeab9a44321
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-10 09:49:46 -0500

    refactor: :sparkles: improve eslint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,8 @@
 import { dirname } from 'path'
 import { fileURLToPath } from 'url'
 import { FlatCompat } from '@eslint/eslintrc'
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended'
+import eslintConfigPrettier from 'eslint-config-prettier'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
@@ -11,12 +13,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
 	...compat.extends('next/core-web-vitals', 'next/typescript', 'prettier'),
-	{
-		rules: {
-			'prettier/prettier': 'error',
-		},
-		plugins: ['prettier'],
-	},
+	eslintPluginPrettierRecommended,
 ]
 
-export default eslintConfig
+export default [...eslintConfig, eslintConfigPrettier]
