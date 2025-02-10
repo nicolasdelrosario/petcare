cff4c89a16f51d530c780bd8c7f45540e7d24987
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-10 08:32:16 -0500

    feat: :sparkles: add action bar component

--- app/dashboard/(pages)/veterinarios/page.tsx
+++ app/dashboard/(pages)/veterinarios/page.tsx
@@ -1,16 +1,14 @@
 // components
 import { HeaderPage } from '../../components/header-page'
 import { MainPage } from '../../components/main-page'
-import { VeterinarianCard } from './veterinarian-card'
-
-// utils
-import { createClient } from '@/lib/supabase/server'
-
-// types
-import { Veterinarian } from '@/types/veterinarian'
+import { VeterinarianCard } from './card'
+import { ActionBar } from './action-bar'
 
 // constants
 import { ROUTES } from '@/constants/routes'
+
+// actions
+import { getVeterinarians } from './actions'
 
 export default async function Veterinarios() {
 	const breadcrumbItems = [
@@ -24,17 +22,13 @@ export default async function Veterinarios() {
 		},
 	]
 
-	const supabase = await createClient()
-	const { data: veterinarians, error } = await supabase
-		.from('veterinarian')
-		.select(`id, name, email, phone, veterinarian_specialization ( id, name ) `)
-		.is('deleted_at', null)
-		.returns<Veterinarian[]>()
+	const veterinarians = await getVeterinarians()
 
 	return (
 		<>
 			<HeaderPage breadcrumbItems={breadcrumbItems} />
 			<MainPage>
+				<ActionBar />
 				<div className='grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
 					{veterinarians?.map(veterinarian => (
 						<VeterinarianCard
