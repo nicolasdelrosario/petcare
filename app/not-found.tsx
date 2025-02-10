32f789aaed6d053d265ed95b452970365f684046
Nicolas Del Rosario <delrosariolozanonicolas@gmail.com>
2025-02-10 11:17:18 -0500

    feat: :recycle: not found must use 'use server' and be exported by default

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,5 +1,7 @@
+'use server'
+
 import { NotFoundPage } from '@/components/not-found-page'
 
-export default function NotFound() {
+export default async function NotFound() {
 	return <NotFoundPage />
 }
